5bcea3dd6ecd331fd553bb44758e4ab08ef55a6e
vk-mithlesh <mithlesh.jha@vyaparkranti.in>
2025-06-13 17:09:23 +0530

    changes
    Signed-off-by: vk-mithlesh <mithlesh.jha@vyaparkranti.in>

--- resources/views/Dashboard/include/asideMenu.blade.php
+++ resources/views/Dashboard/include/asideMenu.blade.php
@@ -115,7 +115,7 @@
               </li>
 
               <li class="menu-item">
-                <a href="{{route("})->name("ContactUsData");")}}" class="menu-link">
+                <a href="{{route("ContactUsData")}}" class="menu-link">
                   <i class="menu-icon tf-icons bx bx-notepad"></i>
                   <div data-i18n="Analytics">Contact us enquiry</div>
                 </a>
