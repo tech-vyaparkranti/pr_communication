fe2246dd538dc5a10052fe424890356cc9546f63
vk-mithlesh <mithlesh.jha@vyaparkranti.in>
2025-06-13 10:12:01 +0530

    slider text section
    Signed-off-by: vk-mithlesh <mithlesh.jha@vyaparkranti.in>

--- resources/views/include/slider.blade.php
+++ resources/views/include/slider.blade.php
@@ -7,44 +7,67 @@
         <div class="swiper-slide">
           <img class="img-fluid" width="" height="" alt="Image" src="{{ asset($slide->image) }}" />
           <div class="video-content">
-          {{-- <!-- <span>Adiyogi Global</span>
-          <h3>Grow with Innovation: Solutions for Sustainable Farming</h3>
-            <a href="{{ route('contactUs') }}" aria-label="Explore The World">Get in touch</a> --> --}}
+            <!-- Dynamic content from the database -->
+            <h2>{!!$slide->heading_top !!}</h2>
+            <h3>{!! $slide->heading_middle !!}</h3>
+            <p>{!! $slide->heading_bottom !!}</p>
+            {{-- Uncomment the following line if you have a contact route for each slide --}}
+            {{-- <a href="{{ route('contactUs') }}" aria-label="Explore The World">Get in touch</a> --}}
           </div>
-        </div> 
+        </div>
         @endforeach
+
+        {{--
+            This block will only render if the $sliders collection is empty.
+            This ensures that either dynamic slides OR static slides are shown, but not both.
+        --}}
+        @unless (count($sliders) > 0)
+        <!-- Static slides - example content added for structure, currently commented out -->
         <div class="swiper-slide">
           <img class="img-fluid" width="" height="" alt="Image" src="./assets/img/Banner Pic 1.jpg" />
           <div class="video-content">
-          <!-- <span>Adiyogi Global</span>
-          <h3>Grow with Innovation: Solutions for Sustainable Farming</h3>
-            <a href="{{ route('contactUs') }}" aria-label="Explore The World">Get in touch</a> -->
+            {{--
+            <h2>Adiyogi Global</h2>
+            <h3>Grow with Innovation: Solutions for Sustainable Farming</h3>
+            <p>A short descriptive text for Banner Pic 1.</p>
+            <a href="{{ route('contactUs') }}" aria-label="Explore The World">Get in touch</a>
+            --}}
           </div>
-        </div> 
+        </div>
         <div class="swiper-slide">
           <img class="img-fluid" width="" height="" alt="Image" src="./assets/img/Banner Pic 2.jpg" />
           <div class="video-content">
-          <!-- <span>Adiyogi Global</span>
-          <h3>Grow with Innovation: Solutions for Sustainable Farming</h3> -->
-            <!-- <a href="{{ route('contactUs') }}" aria-label="Explore The World">Get in touch</a> -->
+            {{--
+            <h2>Adiyogi Global</h2>
+            <h3>Grow with Innovation: Solutions for Sustainable Farming</h3>
+            <p>A short descriptive text for Banner Pic 2.</p>
+            <a href="{{ route('contactUs') }}" aria-label="Explore The World">Get in touch</a>
+            --}}
           </div>
-        </div> 
+        </div>
         <div class="swiper-slide">
           <img class="img-fluid" width="" height="" alt="Image" src="./assets/img/Banner Pic 3.jpg" />
           <div class="video-content">
-          <!-- <span>Adiyogi Global</span>
-          <h3>Grow with Innovation: Solutions for Sustainable Farming</h3> -->
-            <!-- <a href="{{ route('contactUs') }}" aria-label="Explore The World">Get in touch</a> -->
+            {{--
+            <h2>Adiyogi Global</h2>
+            <h3>Grow with Innovation: Solutions for Sustainable Farming</h3>
+            <p>A short descriptive text for Banner Pic 3.</p>
+            <a href="{{ route('contactUs') }}" aria-label="Explore The World">Get in touch</a>
+            --}}
           </div>
-        </div> 
+        </div>
         <div class="swiper-slide">
           <img class="img-fluid" width="" height="" alt="Image" src="./assets/img/Banner Pic 4.jpg" />
           <div class="video-content">
-          <!-- <span>Adiyogi Global</span>
-          <h3>Grow with Innovation: Solutions for Sustainable Farming</h3> -->
-            <!-- <a href="{{ route('contactUs') }}" aria-label="Explore The World">Get in touch</a> -->
+            {{--
+            <h2>Adiyogi Global</h2>
+            <h3>Grow with Innovation: Solutions for Sustainable Farming</h3>
+            <p>A short descriptive text for Banner Pic 4.</p>
+            <a href="{{ route('contactUs') }}" aria-label="Explore The World">Get in touch</a>
+            --}}
           </div>
-        </div> 
+        </div>
+        @endunless
       </div>
     </div>
   </div>
